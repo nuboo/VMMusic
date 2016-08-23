e836d5f4d64f47667a0db5f109236d914a11499d
nuboo <1134533177@qq.com>
2016-08-23 11:03:31 +0800

    添加 下拉刷新，上拉加载库

--- VMMusic/src/com/example/vmmusic/app/fragment/SpecialFragment.java
+++ VMMusic/src/com/example/vmmusic/app/fragment/SpecialFragment.java
@@ -1,89 +1,114 @@
 package com.example.vmmusic.app.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.example.vmmusic.R;
-import com.example.vmmusic.app.activity.MusicListActivity;
 import com.example.vmmusic.app.adapter.SpecialAdapter;
+import com.example.vmmusic.app.customview.layout.SwipeRefreshLayoutFinal;
 import com.example.vmmusic.app.model.Special;
-import com.example.vmmusic.app.utils.TopSettiings;
+import com.example.vmmusic.app.myInterface.Constant;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import cn.finalteam.loadingviewfinal.OnLoadMoreListener;
+import cn.finalteam.loadingviewfinal.RecyclerViewFinal;
 
 /**
  * 专题
  * Created by awx19 on 2016/4/7.
  */
 public class SpecialFragment extends Fragment {
+    List<Special> list = new ArrayList<>();
+    @InjectView(R.id.special_list_view)
+    RecyclerViewFinal specialListView;
+    Integer[] img = new Integer[]{R.drawable.topic, R.drawable.topci_one, R.drawable.topci_two, R.drawable.topci_five, R.drawable.topci_three, R.drawable.topci_four};
+    String[] name = new String[]{"前奏无敌 简单的调调", "疯狂动物城，听完不舍离场的片尾曲", "磁性女声英翻", "动听女生-电音Mire", "那些独立的小清新", "那些好听的明星翻唱"};
+    String[] type = new String[]{"#摇滚", "#电影", "#摇滚", "#爵士", "#情绪", "#流行"};
+    @InjectView(R.id.swipe_ly)
+    SwipeRefreshLayoutFinal swipeLy;
 
-
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_special, null);
-        initView(view);
-      //  topSetting(view);
+        ButterKnife.inject(this, view);
+        initView();
         return view;
     }
 
     /**
      * 初始化控件
-     *
-     * @param view
      */
-    private void initView(View view) {
-        ListView listView = (ListView) view.findViewById(R.id.special_list_view);
-        SpecialAdapter specialAdapter = new SpecialAdapter(view.getContext(), getDate());
-        listView.setAdapter(specialAdapter);
+    private void initView() {
+        SpecialAdapter specialAdapter = new SpecialAdapter(getContext(), getDate());
+        //设置布局样式LayoutManager
+        specialListView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        specialListView.setAdapter(specialAdapter);
+        swipeLy.setColorSchemeResources(R.color.refresh);
+        specialListView.setHasLoadMore(true);
+        setSwipeRefreshInfo();
     }
 
+
+    private void setSwipeRefreshInfo() {
+        swipeLy.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mHandler.sendEmptyMessageDelayed(Constant.CONSTANT101, 2000);
+            }
+        });
+        specialListView.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void loadMore() {
+                mHandler.sendEmptyMessageDelayed(Constant.CONSTANT102, 2000);
+            }
+        });
+        swipeLy.autoRefresh();
+    }
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case Constant.CONSTANT101:
+                    swipeLy.onRefreshComplete();
+                    break;
+                case Constant.CONSTANT102:
+                    specialListView.onLoadMoreComplete();
+                    break;
+            }
+        }
+    };
+
     /**
-  
-    /**
+     * /**
      * 假数据
      *
      * @return
      */
     private List<Special> getDate() {
-        List<Special> list = new ArrayList<Special>();
-        Special special = new Special();
-        special.setSpecial_img(R.drawable.topic);
-        special.setSpecial_name("前奏无敌 简单的调调");
-        special.setSpecial_type("#摇滚");
-        list.add(special);
-        Special special_one = new Special();
-        special_one.setSpecial_img(R.drawable.topci_one);
-        special_one.setSpecial_name("疯狂动物城，听完不舍离场的片尾曲");
-        special_one.setSpecial_type("#电影");
-        list.add(special_one);
-        Special special_two = new Special();
-        special_two.setSpecial_img(R.drawable.topci_two);
-        special_two.setSpecial_name("磁性女声英翻");
-        special_two.setSpecial_type("#摇滚");
-        list.add(special_two);
-        Special special_three = new Special();
-        special_three.setSpecial_img(R.drawable.topci_five);
-        special_three.setSpecial_name("动听女生-电音Mire");
-        special_three.setSpecial_type("#爵士");
-        list.add(special_three);
-        Special special_four = new Special();
-        special_four.setSpecial_img(R.drawable.topci_three);
-        special_four.setSpecial_name("那些独立的小清新");
-        special_four.setSpecial_type("#情绪");
-        list.add(special_four);
-        Special special_five = new Special();
-        special_five.setSpecial_img(R.drawable.topci_four);
-        special_five.setSpecial_name("那些好听的明星翻唱");
-        special_five.setSpecial_type("#流行");
-        list.add(special_five);
+        for (int i = 0; i < img.length; i++) {
+            Special special = new Special();
+            special.setSpecial_img(img[i]);
+            special.setSpecial_name(name[i]);
+            special.setSpecial_type(type[i]);
+            list.add(special);
+        }
         return list;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
---
+++ VMMusic/src/com/example/vmmusic/app/myInterface/Constant.java
@@ -0,0 +1,13 @@
+package com.example.vmmusic.app.myInterface;
+
+/**
+ * Created by Administrator on 2016/8/23.
+ */
+public interface Constant {
+    int CONSTANT101 = 0X101;
+    int CONSTANT102 = 0X102;
+    int CONSTANT103 = 0X103;
+    int CONSTANT104 = 0X104;
+    int CONSTANT105 = 0X105;
+    int CONSTANT106 = 0X106;
+}
